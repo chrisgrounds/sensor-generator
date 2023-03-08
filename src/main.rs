21aaecd438f71a6432aa56c34f9c8dc45936e830
Chris Bacon <chrisbacon2009@hotmail.com>
2023-03-08 14:47:03 +0000

    Generator random data through range

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use csv::Writer;
 use rand::distributions::{Distribution, Standard};
 use rand::Rng;
+use serde::Serialize;
+use std::error::Error;
+use std::fs::File;
 use strum::EnumCount;
 use strum_macros::{EnumCount as EnumCountMacro, EnumIter};
 
-#[derive(Debug, EnumCountMacro, EnumIter)]
+#[derive(Debug, EnumCountMacro, EnumIter, Serialize)]
 pub enum EventType {
     SensorTriggered,
     SensorAcknowledged,
     SensorOff,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct EventData {
     pub event_type: EventType,
     pub timestamp: u64,
@@ -34,8 +38,23 @@ impl Distribution<EventData> for Standard {
     }
 }
 
+fn write_event_to_csv(wtr: &mut Writer<File>, event_data: EventData) -> Result<(), Box<dyn Error>> {
+    wtr.serialize(&event_data)?;
+    wtr.flush()?;
+    Ok(())
+}
+
 fn main() {
+    let total_events = 1000;
     let mut rng = rand::thread_rng();
-    let random_event: EventData = rng.gen();
-    println!("{:?}", random_event);
+    let mut wtr = Writer::from_path("random_data.csv").unwrap();
+
+    for _ in 0..total_events {
+        let random_event: EventData = rng.gen();
+        println!("{:?}", random_event);
+
+        write_event_to_csv(&mut wtr, random_event).unwrap();
+    }
+
+    println!("Done!");
 }
